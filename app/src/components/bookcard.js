05351979a69be2137240fe9f5faa2fcb0c54adaa
Josh PC <josh.rosenhanst@gmail.com>
2019-10-16 20:47:58 -0400

    add year and ratings text display

--- app/src/components/bookcard.js
+++ app/src/components/bookcard.js
@@ -6,7 +6,10 @@ const BookCard = ({ book }) => (
     <div key={book.id} class="w-56 rounded overflow-hidden shadow-lg m-3">
     <img class="w-full" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1414347376l/18050143.jpg" alt="Sunset in the mountains" />
     <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">{ book.title }</div>
+      <div class="font-bold text-xl mb-0">{ book.title }</div>
+      <p className="text-gray-700 font-bold text-sm mb-1">
+        { book.year } | { book.rating }
+      </p>
       <p class="text-gray-700 text-base">
         { book.author }
       </p>
