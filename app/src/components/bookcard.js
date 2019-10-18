3d24c54a6ca29906bdd7b54879e0be45aea80261
Vishnu KS <yo@vishnuks.com>
2019-10-18 19:43:07 +0530

    src: Use shoe dog book as cover photo.

--- app/src/components/bookcard.js
+++ app/src/components/bookcard.js
@@ -4,7 +4,7 @@ import StarRatings from 'react-star-ratings';
 
 const BookCard = ({ book }) => (
     <div className="w-56 rounded overflow-hidden shadow-lg m-3">
-    <img className="w-full" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1414347376l/18050143.jpg" alt="Sunset in the mountains" />
+    <img className="w-full" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1457284880l/27220736.jpg" alt="Sunset in the mountains" />
     <div className="px-6 py-4">
       <div className="font-bold text-xl mb-0">{ book.title }</div>
       <div className="text-gray-700 font-bold text-base mb-1">
