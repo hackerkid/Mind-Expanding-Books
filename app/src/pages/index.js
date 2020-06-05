40d8e368808028449e17030194e72b7411bab270
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2020-06-05 18:03:37 +0545

    Implemented infinite scroll

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,18 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BookCard from "../components/bookcard"
 
+function myFunction(data, end, setEnd) {
+  if (document.documentElement.clientHeight+document.documentElement.scrollTop===document.documentElement.scrollHeight) {
+    setEnd(end+4)
+  }
+}
+
 export default ({data}) => {
+  let [end, setEnd] = useState(4);
+  useEffect(()=>{
+    window.document.onscroll = () => myFunction(data, end, setEnd);
+  })  
   return (
     <Layout>
       <SEO title="Home" />
       <div className="flex flex-row flex-wrap">
-      {data.allBooksJson.edges.map(function(x) {
-        return (
-          <BookCard book={x.node} key={x.node.id} />
-        )
+      {data.allBooksJson.edges.map(function(x, index) {
+        if(index<end){
+          return (
+            <BookCard book={x.node} key={x.node.id} />
+          )
+        }        
       })}
       </div>
     </Layout>
