4d81bf3fe1266100bbca17c7cea95fedee1c245b
Vishnu KS <yo@vishnuks.com>
2020-08-23 00:58:19 +0530

    Configure eslint.

---
+++ app/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
