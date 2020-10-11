ccd888bce9d77f275917b8f392f0c3766b30dc83
Carl Dungca <carl.dungca@yahoo.com>
2020-10-11 12:56:47 -0700

    Clicking bookmark button saves book information to a localStorage array

--- app/src/components/bookmark.js
+++ app/src/components/bookmark.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import { Button } from "react-bootstrap"
 
+const saveBookToLocalStorage = (book) => {
+	if (typeof window !== undefined) {
+		const readingList = localStorage.getItem('Reading List')
+		let listData
+
+		if (readingList === null) {
+			listData = []
+		} else {
+			listData = JSON.parse(readingList)
+		}
+
+		listData.push(book)
+
+		localStorage.setItem('Reading List', JSON.stringify(listData))
+	}
+}
+
 export default ({ book }) => {
 	return (
 		<Button variant="secondary">
-			<span onClick={() => console.log(book)}>
+			<span onClick={() => saveBookToLocalStorage(book)}>
 				Add to reading list
 			</span>
 		</Button>
