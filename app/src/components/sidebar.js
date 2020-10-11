ac311138c7ee6a6bb47034426c2ac96a62d12f1b
palak199 <p.thapar99@gmail.com>
2020-10-11 09:11:34 +0530

    remobed unused vars

--- app/src/components/sidebar.js
+++ app/src/components/sidebar.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react"
+import React from "react"
 import { Nav } from "react-bootstrap"
 import { StaticQuery, graphql } from "gatsby"
 import "../styles/sidebar.css"
