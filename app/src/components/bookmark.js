b8db84db741224eb4d038f6d91ae6c59f9aa32d4
Carl Dungca <carl.dungca@yahoo.com>
2020-10-11 13:25:53 -0700

    Can now toggle books in and out of localStorage

--- app/src/components/bookmark.js
+++ app/src/components/bookmark.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Button } from "react-bootstrap"
 
-const saveBookToLocalStorage = (book) => {
+const toggleLocalStorage = (book) => {
 	if (typeof window !== undefined) {
 		const readingList = localStorage.getItem('Reading List')
 		let listData
 		let bookId = book.id
 
+		// If no booklist exists
 		if (readingList === null) {
 			listData = {}
 		} else {
 			listData = JSON.parse(readingList)
 		}
 
-		listData[bookId] = book
+		// Write or delete from localStorage if duplicate
+		if (Object.keys(listData).includes(bookId)) {
+			console.log('exists')
+			delete listData[bookId]
+		} else {
+			listData[bookId] = book
+		}
 
 		localStorage.setItem('Reading List', JSON.stringify(listData))
 	}
@@ -22,7 +29,7 @@ const saveBookToLocalStorage = (book) => {
 export default ({ book }) => {
 	return (
 		<Button variant="secondary">
-			<span onClick={() => saveBookToLocalStorage(book)}>
+			<span onClick={() => toggleLocalStorage(book)}>
 				Add to reading list
 			</span>
 		</Button>
