7284e0d5aa79ac0c482af78f75c2c816acdf0283
Carl Dungca <carl.dungca@yahoo.com>
2020-10-11 14:34:40 -0700

    Reverted previous 4 commits so that data isn't directly written to localStorage

--- app/src/components/bookmark.js
+++ app/src/components/bookmark.js
@@ -1,35 +1,10 @@
 import React from 'react'
 import { Button } from "react-bootstrap"
 
-const toggleLocalStorage = (book) => {
-	if (typeof window !== undefined) {
-		const readingList = localStorage.getItem('Reading List')
-		let listData
-		let bookId = book.id
-
-		// If no booklist exists
-		if (readingList === null) {
-			listData = {}
-		} else {
-			listData = JSON.parse(readingList)
-		}
-
-		// Write or delete from localStorage if duplicate
-		if (Object.keys(listData).includes(bookId)) {
-			console.log('exists')
-			delete listData[bookId]
-		} else {
-			listData[bookId] = book
-		}
-
-		localStorage.setItem('Reading List', JSON.stringify(listData))
-	}
-}
-
 export default ({ book }) => {
 	return (
 		<Button variant="secondary">
-			<span onClick={() => toggleLocalStorage(book)}>
+			<span onClick={() => console.log(book)}>
 				Add to reading list
 			</span>
 		</Button>
