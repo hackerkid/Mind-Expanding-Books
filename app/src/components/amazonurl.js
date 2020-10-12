1bf5c609436a3d8ac6400472328fd20fabf6f9a3
Elysia Ong <elysia.owx@gmail.com>
2020-10-12 16:38:50 +0800

    Fix typo in method name

--- app/src/components/amazonurl.js
+++ app/src/components/amazonurl.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { OutboundLink } from "gatsby-plugin-google-analytics"
 
-const getTaggetURL = (book) => {
+const getTargetURL = (book) => {
     return book.amazon_url + "?tag=vishnuks-20";
 }
 
 export default ({ book }) => {
     return (
-        <OutboundLink href={getTaggetURL(book)} target="_blank">
+        <OutboundLink href={getTargetURL(book)} target="_blank">
             <img alt="Amazon link" style={{ "width": "30px" }} src="https://img.icons8.com/color/48/000000/amazon.png"/>
         </OutboundLink>
     )
