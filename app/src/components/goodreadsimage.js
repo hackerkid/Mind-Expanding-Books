f34b0a8ec1f9e9b3d95ae26d46d55769f7e0fa04
Elysia Ong <elysia.owx@gmail.com>
2020-10-12 16:39:15 +0800

    Use goodreads icon

--- app/src/components/goodreadsimage.js
+++ app/src/components/goodreadsimage.js
@@ -16,9 +16,9 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "goodreads.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 30) {
             ...GatsbyImageSharpFluid
           }
         }
