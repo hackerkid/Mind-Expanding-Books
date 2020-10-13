6fb5c211533934ff34b0db8e73523e51cc829cde
bohemianjock <appu.yess@gmail.com>
2020-10-13 13:34:42 +0530

    amazon-url undefined error fix

--- app/src/components/amazonurl.js
+++ app/src/components/amazonurl.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
-
-const propTypes = {
-  book: {},
-
-};
+import PropTypes from 'prop-types';
 
 const getTargetURL = (book) => `${book.amazon_url}?tag=vishnuks-20`;
 
-export default () => (
-  <OutboundLink href={getTargetURL(propTypes.book)} target="_blank">
-    <img alt="Amazon link" style={{ marginBottom: '-8px' }} src="https://img.icons8.com/color/48/000000/amazon.png" />
+const AmazonURL = ({ book }) => (
+  <OutboundLink href={getTargetURL(book)} target="_blank">
+    <img
+      alt="Amazon link"
+      style={{ marginBottom: '-8px' }}
+      src="https://img.icons8.com/color/48/000000/amazon.png"
+    />
   </OutboundLink>
 );
+
+AmazonURL.propTypes = {
+  book: PropTypes.shape(),
+};
+
+AmazonURL.defaultProps = {
+  book: {},
+};
+
+export default AmazonURL;
