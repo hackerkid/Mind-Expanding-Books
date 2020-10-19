4c4b4a490d63a5f00ae098ffb95afc1f5794f908
Jackson Isaac <jacksonisaac2008@gmail.com>
2020-10-19 21:46:54 +0200

    Fix typo in sample config file

--- utils/config-sample.py
+++ utils/config-sample.py
@@ -1,4 +1,4 @@
 # save this file as 'config.py' and then, fill it with you api key
 GOODREADS_PUBLIC_API_KEY = "write here your goodreads public API key"
 GOOGLE_SEARCH_RAPIDAPI_HOST = ""
-GOOGLESEARCH_RAPIDAPI_KEY = ""
+GOOGLE_SEARCH_RAPIDAPI_KEY = ""
