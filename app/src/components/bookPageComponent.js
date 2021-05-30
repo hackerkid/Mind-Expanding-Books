1bcfa6cbf91d68ea371c77fcf8bcb698980120e3
TetianaHrunyk <tetiana.hrunyk@gmail.com>
2021-05-30 11:41:59 +0300

    edit basic page for individual books

--- app/src/components/bookPageComponent.js
+++ app/src/components/bookPageComponent.js
@@ -1,12 +1,83 @@
+import React from "react"
 import "../styles/sidebar.css"
 import BookCard from "./bookcard"
 import { useState, useEffect } from "react"
-import React from "react"
+import StarRatings from "react-star-ratings"
 import Layout from "./layout"
 import SEO from "./seo"
 import SideBar from "./sidebar"
-import { Container, Row, Col, Navbar } from "react-bootstrap"
+import { Card, Row, Col } from "react-bootstrap"
+import AmazonURL from "../components/amazonurl"
+import Bookmark from "../components/bookmark"
+import GoodReadsImage from "../components/goodreadsimage"
 
 export default ({ book }) => {
-  return <BookCard book={book} />
+  return (
+    <>
+      <Card style={{ marginBottom: "15px", borderWidth: "0rem" }}>
+        <Row>
+          <Col xs={6} sm={6} md={4} xl={2}>
+            <Card.Img
+              style={{
+                paddingLeft: "15px",
+                paddingRight: "15px",
+                paddingTop: "30px",
+              }}
+              src={book.image_url}
+              alt={book.title}
+            />
+          </Col>
+          <Col xs={12} sm={6} md={8} xl={10}>
+            <Card.Body>
+              <Card.Title>{book.title}</Card.Title>
+              <Card.Subtitle className="text-muted">
+                <Card.Text style={{ paddingTop: "2px" }}>
+                  {book.author} <b>{book.year ? book.year : null}</b>
+                </Card.Text>
+                <StarRatings
+                  rating={parseFloat(book.rating)}
+                  numberOfStars={5}
+                  starDimension="18px"
+                  starSpacing="1px"
+                  starRatedColor="#fa604a"
+                />
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    paddingTop: ".75rem",
+                  }}
+                >
+                  <div
+                    style={{
+                      width: "30px",
+                      height: "30px",
+                      marginRight: "5px",
+                    }}
+                  >
+                    {book.amazon_url ? <AmazonURL book={book} /> : null}
+                  </div>
+                  <div style={{ width: "30px", height: "30px" }}>
+                    <a href={book.url}>
+                      <GoodReadsImage />
+                    </a>
+                  </div>
+                  <Bookmark book={book} />
+                </div>
+              </Card.Subtitle>
+              <p
+                style={{
+                  color: "gray",
+                  fontSize: "0.8rem",
+                  paddingTop: "1rem",
+                }}
+              >
+                {book.description}
+              </p>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </>
+  )
 }
